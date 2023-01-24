2143372857f0d686576df573ba8407dca17b56b0
joseLeiva <106642804+JLeiva44@users.noreply.github.com>
2023-01-24 21:53:30 +0100

    Actualization map

---
+++ world/csp.py
@@ -0,0 +1,196 @@
+
+from itertools import count
+from operator import eq, neg
+from sortedcontainers import SortedSet
+import random
+import search
+
+
+def count(seq):
+    """Count the number of items in sequence that are interpreted as true."""
+    return sum(map(bool, seq))
+
+def first(iterable, default=None):
+    """Return the first element of an iterable; or default."""
+    return next(iter(iterable), default)
+
+class CSP(search.Problem):
+    """This class describes a finite-domain Constraint Satisfaction Problem
+    A CSP is specified by the following inputs:
+        - variables : A list of variables; each is atomic
+        - domains : A dict { var : [posible_values]}
+        - neighbors : A dict {var: [var_2,...,var_n]} if var and var_i participate in the same constraint
+        - constainst : A function f(A,a,B,b) that return true if neighbors A, B 
+        satisfy the constraint when A=a, B=b
+    """
+    def __init__(self,variables,domains,neighbors,constraints) -> None:
+        super().__init__(())
+        variables = variables or list(domains.keys())
+        self.variables = variables
+        self.domains = domains
+        self.neighbors = neighbors
+        self.constraints = constraints
+        self.curr_domains = None
+        self.nassigns = 0
+
+    def assign(self, var, val, assignment):
+        """Add {var: value} to assignment; Discard the old value if any"""
+        assignment[var] = val
+        self.nassigns+=1
+
+    def unassign(self,var,assignment) :
+        """Remove {var: val} from assignment
+        Do not use this for changing a variable to a new value"""
+        if var in assignment:
+            del assignment[var]
+
+    def nconflicts(self,var,val,assignment):
+        """Returns the number of conflicts var = val has with other variables"""
+        def conflict(var2):
+            return var2 in assignment and not self.constraints(var,val,var2,assignment[var2])
+        return count(conflict(v) for v in self.neighbors[var])            
+
+    def acions(self, state):
+        """Returns a list pf applicable actions : non conflicting assignments to an unassigned variable"""
+        if len(state) == len(self.variables): return []
+        else:
+            assignment = dict(state)
+            var = first([v for v in self.variables if v not in assignment])
+            return [(var,val) for val in self.domains[var] if self.nconflicts(var,val,assignment) == 0]
+
+    def result(self, state, action):
+        """Performs an action and return the new state"""
+        (var,val) = action
+        return state + ((var,val),)
+
+    def is_goal(self, state):
+        """The goal test is to assign all variables, with all constraints satisfied"""
+        assignment = dict(state)
+        return (len(assignment) == len(self.variables) and all(self.nconflicts(variables, assignment[variables], assignment) == 0 for variables in self.variables))
+
+    # for constraint propagation
+    def support_prunning(self):
+        """Make sure we can prune values from domains."""
+        if self.curr_domains is None:
+            self.curr_domains = {v: list(self.domains[v]) for v in self.variables}
+
+    def prune(self,var,value,removals):
+        """Rule out var = value"""
+        self.curr_domains[var].remove(value)
+        if removals is not None:
+            removals.append((var,value))   
+
+    def choices(self, var):
+        """Return all values for var that aren't currently ruled out."""
+        return (self.curr_domains or self.domains)[var]        
+
+    def infer_assignment(self):
+        """Returns the partial assignment implied by the current inferences""" 
+        self.support_prunning()
+        return {v: self.curr_domains[v][0] for v in self.variables if 1 == len(self.curr_domains[v])}    
+
+    def display(self,assignment):
+        """Shows a human-readable representation of the csp"""
+        print(assignment)                    
+
+    def suppose(self, var, value):
+        """Start accumulating inferences from assuming var=value."""
+        self.support_prunning()
+        removals = [(var, a) for a in self.curr_domains[var] if a != value]
+        self.curr_domains[var] = [value]
+        return removals   
+
+        
+    def restore(self, removals):
+        """Undo a supposition and all inferences from it."""
+        for B, b in removals:
+            self.curr_domains[B].append(b)    
+
+    def h(self, node):
+        pass          
+
+def no_arc_heuristic(csp, queue):  return queue    
+
+def dom_j_up(csp, queue):
+    return SortedSet(queue, key = lambda t: neg(len(csp.curr_domains[t[1]])))
+
+def AC3(csp, queue = None, removals = None, arc_heuristic = dom_j_up):
+    if queue is None:
+        queue = {(Xi,Xk) for Xi in csp.variables for Xk in csp.neighbors[Xi]}
+
+    csp.support_prunning()
+    queue = arc_heuristic(csp, queue)
+    checks = 0
+    while queue :
+        (Xi,Xj) = queue.pop()
+        revised,checks = revise(csp,Xi,Xj,removals,checks)
+        if revised:
+            if not csp.curr_domains[Xi]:
+                return False, checks # CSP is inconsistent
+            for Xk in csp.neighbors[Xi]:
+                if Xk != Xj:
+                    queue.add((Xk,Xi))
+    return True,checks # Csp is satisfiable       
+
+def revise(csp,Xi,Xj,removals, checks = 0):
+    revised = False
+    for x in csp.curr_domains[Xi][:]:
+        conflict = True
+        for y in csp.curr_domains[Xj]:
+            if csp.constraints(Xi,x,Xj,y):
+                conflict = False
+            checks += 1
+            if not conflict:
+                break   
+        if conflict :
+            csp.prune(Xi,x,removals)
+            revised = True
+    return revised, checks  
+
+def first_unassigned_variable(assignment, csp):
+    """The default variable order."""
+    return first([var for var in csp.variables if var not in assignment])    
+
+def unordered_domain_values(var, assignment, csp):
+    """The default value order."""
+    return csp.choices(var)    
+
+def no_inference(csp, var, value, assignment, removals):
+    return True    
+
+def backtracking_search(csp, select_unassigned_variable=first_unassigned_variable,
+                        order_domain_values=unordered_domain_values, inference=no_inference):
+    """[Figure 6.5]"""
+
+    def backtrack(assignment):
+        if len(assignment) == len(csp.variables):
+            return assignment
+        var = select_unassigned_variable(assignment, csp)
+        for value in order_domain_values(var, assignment, csp):
+            if 0 == csp.nconflicts(var, value, assignment):
+                csp.assign(var, value, assignment)
+                removals = csp.suppose(var, value)
+                if inference(csp, var, value, assignment, removals):
+                    result = backtrack(assignment)
+                    if result is not None:
+                        return result
+                csp.restore(removals)
+        csp.unassign(var, assignment)
+        return None
+
+    result = backtrack({})
+    #assert result is None or csp.is_goal(result)
+    return result
+
+
+class UniversalDict:
+    """Returns the same value for any key"""
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __getitem__(self,key): return self.value
+
+    def __repr__(self) -> str: return '{{Any: {0!r}}}'.format(self.value)
+        
+
+
--- world/map_csp.py
+++ world/map_csp.py
@@ -1,12 +1,10 @@
 import numpy as np
-#from world_ import *
-from world.world_ import *
-#from ..CSP.csp import CSP, backtracking_search
-#from ..CSP.csp import UniversalDict
-#from ..CSP.csp import AC3
+
+from world_ import *
+from collections import defaultdict
 import random
-from world.clases import *
-from CSP.csp import *
+from clases import *
+from csp import *
 
 
 
@@ -14,160 +12,176 @@ def different_values_constraint(A,a,B,b):
     """ A constraint saying two neighboring variables must differ in value"""
     return a!= b        
 
-def in_zone_constraint():
-    """"""   
-    pass
+
 
 def all_diff_constraint(*values):
     """Returns True if all values are different, False otherwise"""
     return len(values) is len(set(values))
 
-def valid_world_constraint(*values):
-    """Returns true if the is a valid world"""   
-    pass 
-
-
-    
 
 def myMapConstraint(A,a,B,b):
     r = random.randint(0,1)
     if r == 0:return different_values_constraint(A,a,B,b) 
     return not different_values_constraint(A,a,B,b)  
 
-# class CivilizationDict():
-#     def __init__(self) -> None:
-#         self.value = None
-#         self.domain0 = [ocean(),mountain()]
-#         self.domain1 = [town(), beach(), ocean(),mountain(),plain(),port(),fish(),mine(),farm(),fruits()]
-#         self.domain2 = [mountain(),mine()]
-#         self.domain3 = [beach(), ocean(), port(), fish(), farm()]
-
-#     def __getitem__(self,key):
-#         zone = key.zone
-#         if zone is None:
-#             self.value = self.domain0
-#             return self.domain0 
-#         elif zone == 'indian':
-#             self.value = self.domain1
-#             return self.domain1
-#         elif zone == 'dessert':
-#             self.value = self.domain2
-#             return self.domain2    
-#         elif zone == 'viking':
-#             self.value = self.domain3
-#             return self.domain3
-               
-
-#     def __repr__(self) -> str: return '{{Any: {0!r}}}'.format(self.value)             
 
 
 
-class map:
-    def __init__(self,players,fill = None,prob_city = None, prob_town = None, prob_mount = None, prod_fruit = None,prod_fish = None):
-        self.size_x= self.size_y = len(players) * 2
-        self.players = players
-        self.nei = dict()
-        self.map = np.ndarray((self.size_x,self.size_y),dtype=environment_things)
-        for i in range(self.map.shape[0]):
-            for j in range(self.map.shape[1]):
-#                self.map[i,j] = environment_things(i,j)
-                self.map[i,j] = environment_things()
+class map(CSP):
+    def __init__(self,size_x, size_y,civilizations,fill = None,prob_city = None, prob_town = None, prob_mount = None, prod_fruit = None,prod_fish = None):
+        self.size_x= size_x
+        self.size_y = size_y
+        self.civilizations = civilizations
+
+        self.map = np.ndarray((size_x,size_y), dtype = environment_things)
+        self.map.fill(environment_things)
+
+        cities = self.build_cities(civilizations)
+
+        neighbors = self.agroup_neighbors(cities)
+
+        domain = [town(), beach(), ocean(),mountain(),plain(),port(),fish(),mine(),farm(),fruits(), planting()]
+        #domain = [2,3,4,5,6,7,8,9,10,11,12]
+        domains = UniversalDict(domain)
+
+        m =CSP(list(neighbors.keys()), domains,neighbors, myMapConstraint)
+        assignments = backtracking_search(m)
+        for assignmnent in assignments:
+            value = assignments[assignmnent]
+            x,y = assignmnent
+            self.map[x,y] = value
+
+
+
         
-        self.neighbors = self.parse_neighbors(self.map)   
-        domain = [town(), beach(), ocean(),mountain(),plain(),port(),fish(),mine(),farm(),fruits()]
-        self.domains = UniversalDict(domain)
-        #self.domains2 = CivilizationDict()
-
-        self.world = self.generation_world_csp()
 
 
-    def parse_neighbors(self,map):
-            def calculate_adyacents(map,x,y):
-                dx = np.array([-1,1,0,0,-1,1,-1,1]) #up down left right diag izq diag der
-                dy = np.array([0,0,-1,1,-1,-1,1,1])  
-                adyacents = []      
-
-                for i in range(len(dx)):
-                    new_x = x + dx[i]
-                    new_y = y + dy[i]
-                    if (new_x >= 0 and new_y >= 0 and new_x < map.shape[0] and new_y < map.shape[1]):
-                        adyacents.append(map[new_x,new_y])
-
-                return adyacents    
-            neighbors = dict()
-            for i in range(map.shape[0]):
-                for j in range(map.shape[1]):
-                    neighbors[map[i,j]] = calculate_adyacents(map,i,j)
-            return neighbors        
-    
     
     def __str__(self):
         s=''
-        for i in range(self.world.shape[0]):
-            for j in range(self.world.shape[1]):
-                if isinstance(self.world[i,j],ocean):
-                    s +='O '
-                elif isinstance(self.world[i,j],mountain):
-                    s +='M '
-                elif isinstance(self.world[i,j],fruits):
-                    s +='F '
-                elif isinstance(self.world[i,j],beach):
-                    s +='B '
-                elif isinstance(self.world[i,j],city):
-                    s +='X '
-                elif isinstance(self.world[i,j],town):
-                    s +='T '
-                elif isinstance(self.world[i,j],fish):
-                    s +='P '
-                else:
-                    s+='L '
+        for i in range(self.size_x):
+            for j in range(self.size_y):
+                if isinstance(self.map[i,j],environment_things):
+                    s+="_"
+
+                if isinstance(self.map[i,j],ocean):
+                    s +='O'
+                elif isinstance(self.map[i,j],mine):
+                    s+='K'
+                elif isinstance(self.map[i,j],fruits):
+                    s +='F'
+                elif isinstance(self.map[i,j],city):
+                    s +='X'
+                elif isinstance(self.map[i,j],town):
+                    s +='T'
+                elif isinstance(self.map[i,j],fish):
+                    s +='P'
+                elif isinstance(self.map[i,j],farm):
+                    s +='G'
+                elif isinstance(self.map[i,j],planting):
+                    s+='Y'
+                elif isinstance(self.map[i,j], port):
+                    s+='R'
+                elif isinstance(self.map[i,j],plain):
+                    s+='L'
+                elif isinstance(self.map[i,j],mountain):
+                    s +='M'
+                elif isinstance(self.map[i,j],beach):
+                    s +='B'
+
+                    
+                
+
+                if self.map[i,j].soldado == None:
+                    s+='_ '
+                elif isinstance(self.map[i,j].soldado,Guerrero):
+                    s+='g '
+                elif isinstance(self.map[i,j].soldado,Defensor):
+                    s+='d '
+                elif isinstance(self.map[i,j].soldado,Espadachin):
+                    s+='e '
             s+='\n'
         return s
 
-    def make_zones(self,world,players):
-        def propagate_zone(x,y,world,civ):
-            dr = [-1,-1,-1,0,0,1,1,1]
-            dc = [-1,0,1,-1,1,-1,0,1]
-            for i in range(len(dr)):
-                new_x = x + dr[i]
-                new_y = y + dc[i]
-                if new_x > 0 and new_y > 0 and new_x < world.shape[0] and new_y < world.shape[1]:
-                    world[new_x,new_y].zone = civ
-            return world        
+    def build_cities(self, civilizations):
+        cities = []
+        def build_city(pos,cities,civ,i) :
+            x,y = pos
+            c =city(civilizations[i],x,y)
+            self.map[x,y] = c
+            cities.append(c)
             
-        cities_loc = []
-        x = -1
-        y = -1
-        for player in players:
-            civ = player.civilization
-            c = city(civ,0,0)
-            #c.zone = civ
-            
-            while (len(cities_loc) > 0 and (x,y) not in cities_loc):
-                x = random.randint(0,world.shape[0] -1)
-                y = random.randint(0,world.shape[1] - 1)
-            c.row=x
-            c.col=y
-            world [x,y] = c
-            world = propagate_zone(x,y,world,civ)
-            cities_loc.append((x,y))
-        return world    
+        for i in range(len(civilizations)):
+            x = random.randint(0,self.map.shape[0]-1)
+            y = random.randint(0,self.map.shape[1]-1)
+            pos = (x,y)
+            build_city(pos, cities,civilizations,i)
+        return cities    
+
+   
+
+
+    def agroup_neighbors(self,cities):
+        # solo participan en la restriccion los alrededores de las ciudades
+        def buscar_posiciones_aledanas(x,y,cantidad):
+            domain = [town(), beach(), ocean(),mountain(),plain(),port(),fish(),mine(),farm(),fruits(), planting()]
+            dx = np.array([-1,1,0,0,-1,1,-1,1]) #up down left right diag izq diag der
+            dy = np.array([0,0,-1,1,-1,-1,1,1])  
+            result = []
+            for i in range(len(dx)):
+                if cantidad == 0: break
+                new_x = dx[i] +x
+                new_y= dy[i] + y
+                if new_x > 0 and new_y > 0 and new_x < self.map.shape[0] and new_y < self.map.shape[0]:
+                    if not isinstance(self.map[new_x, new_y], city) and not isinstance(self.map[new_x,new_y], ocean) :
+                        result.append((new_x,new_y))
+                        self.map[new_x,new_y] = ocean # ya esta marcado
+                        x = new_x
+                        y = new_y
+                        cantidad -=1 
+            return result               
+
+                
+                
+
+        neighbors = defaultdict(list)
+        variables = []
+        for cty in cities:
+            x = cty.row
+            y = cty.col
+            # las posiciones del asentaminto
+            r = random.randint(1,5)
+            setlement_pos = buscar_posiciones_aledanas(x,y,r)
+            for p in setlement_pos:
+                for q in setlement_pos:
+                    if p!=q:
+                        neighbors[p].append(q)
+        return neighbors        
 
 
 
-    def generation_world_csp(self):
-        world = np.full((self.size_x,self.size_y),environment_things(), dtype = environment_things)
-        neighbors = self.parse_neighbors(world)
-        world = self.make_zones(world,self.players) # reparte las ciudades de las civilizaciones
+    # def generation_world_csp(self):
+    #     world = np.full((self.size_x,self.size_y),environment_things(), dtype = environment_things)
+    #     neighbors = self.parse_neighbors(world)
+    #     world = self.make_zones(world,self.players) # reparte las ciudades de las civilizaciones
         
-        #d = CivilizationDict()
-        map_csp = CSP(list(self.neighbors.keys()), self.domains,self.neighbors, myMapConstraint)  
-        assignments = backtracking_search(map_csp)
-        for cell in assignments.keys():
-            x = cell.x
-            y = cell.y
-            world[x,y] = assignments[cell]
+    #     #d = CivilizationDict()
+    #     map_csp = CSP(list(self.neighbors.keys()), self.domains,self.neighbors, myMapConstraint)  
+    #     assignments = backtracking_search(map_csp)
+    #     for cell in assignments.keys():
+    #         x = cell.x
+    #         y = cell.y
+    #         world[x,y] = assignments[cell]
 
-        return world 
+    #     return world 
+
+
+# probando el mapa
+
+mapa = map(10,10,['viking', 'indians', 'mexicans'])
+
+
+
+print(mapa)
 
 
